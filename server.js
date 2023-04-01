82476d77e986bb16082b37f190bc729e8820b5ad
Alexiatech <alexiawiersma@gmail.com>
2023-04-01 16:21:51 +0200

    routes

--- server.js
+++ server.js
@@ -6,42 +6,22 @@ const port = 4000;
 const bodyParser = require('body-parser');
 
 
-// Importeer routebestanden
-const alexiaRoutes = require('./routes/alexiaroutes');
-const larsRoutes = require('./routes/larsroutes');
-const lynnRoutes = require('./routes/lynnroutes');
-const thijmenRoutes = require('./routes/thijmenroutes');
-const youriRoutes = require('./routes/youriroutes');
-const algemeneroutes = require('./routes/algemeneroutes');
+
+
+// Connectie
+const PORT = process.env.PORT || port;
+app.listen(PORT, console.log(`Running on port: ${PORT}`));
+
+
+
 
 // Middleware
 app.use(express.static('static'));
 app.use(bodyParser.urlencoded({ extended: true }));
 require('dotenv').config({ path: '.env' });
 
-// Gebruik EJS als de standaard view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
 
-// Gebruik de geïmporteerde routes als middleware
-app.use(alexiaRoutes);
-app.use(larsRoutes);
-app.use(lynnRoutes);
-app.use(thijmenRoutes);
-app.use(youriRoutes);
-app.use(algemeneroutes);
 
-// Error 404
-app.use(function (req, res) {
-  res.locals.title = "Error 404"
-  res.status(404).render('404', {
-    path: 'Error'
-  });
-});
-
-// Connectie
-const PORT = process.env.PORT || port;
-app.listen(PORT, console.log(`Running on port: ${PORT}`));
 
 // Database
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -57,4 +37,47 @@ client.connect()
   .catch((err) => console.log('@@-- error', err));
 
 
+
+
+// Exporteer het client object
+module.exports = {
+  client
+};
+
+
+
+
+// Gebruik EJS als de standaard view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+// Importeer routebestanden
+const alexiaRoutes = require('./routes/alexiaroutes');
+const larsRoutes = require('./routes/larsroutes');
+const lynnRoutes = require('./routes/lynnroutes');
+const thijmenRoutes = require('./routes/thijmenroutes');
+const youriRoutes = require('./routes/youriroutes');
+const algemeneroutes = require('./routes/algemeneroutes');
+
+
+
+
+// Gebruik de geïmporteerde routes als middleware
+app.use(alexiaRoutes);
+app.use(larsRoutes);
+app.use(lynnRoutes);
+app.use(thijmenRoutes);
+app.use(youriRoutes);
+app.use(algemeneroutes);
+
+
+
+// Error 404
+app.use(function (req, res) {
+  res.locals.title = "Error 404"
+  res.status(404).render('404', {
+    path: 'Error'
+  });
+});
 
