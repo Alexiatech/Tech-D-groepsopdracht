842de43a25bee97f87c81cbb2364c4e31c7e89fb
Lynn Wolters <lynn.wolters27@gmail.com>
2023-04-02 14:32:08 +0200

    Mijn feature gebouwd

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Require DEPENDENCIES
+// require dependecies
 const express = require('express');
 const app = express();
 const { engine } = require('ejs');
@@ -6,44 +6,24 @@ const port = 4000;
 const bodyParser = require('body-parser');
 
 
-// Importeer routebestanden
-const alexiaRoutes = require('./routes/alexiaroutes');
-const larsRoutes = require('./routes/larsroutes');
-const lynnRoutes = require('./routes/lynnroutes');
-const thijmenRoutes = require('./routes/thijmenroutes');
-const youriRoutes = require('./routes/youriroutes');
-const algemeneroutes = require('./routes/algemeneroutes');
 
-// Middleware
+
+// connectie
+const PORT = process.env.PORT || port;
+app.listen(PORT, console.log(`Running on port: ${PORT}`));
+
+
+
+
+// middleware
 app.use(express.static('static'));
 app.use(bodyParser.urlencoded({ extended: true }));
 require('dotenv').config({ path: '.env' });
 
-// Gebruik EJS als de standaard view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
 
-// Gebruik de geïmporteerde routes als middleware
-app.use(alexiaRoutes);
-app.use(larsRoutes);
-app.use(lynnRoutes);
-app.use(thijmenRoutes);
-app.use(youriRoutes);
-app.use(algemeneroutes);
 
-// Error 404
-app.use(function (req, res) {
-  res.locals.title = "Error 404"
-  res.status(404).render('404', {
-    path: 'Error'
-  });
-});
 
-// Connectie
-const PORT = process.env.PORT || port;
-app.listen(PORT, console.log(`Running on port: ${PORT}`));
-
-// Database
+// database
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.DB_CONNECTION_STRING;
 
@@ -55,6 +35,56 @@ const client = new MongoClient(
 client.connect()
   .then((res) => console.log('@@-- connection established', res))
   .catch((err) => console.log('@@-- error', err));
+
+
+
+
+// exporteer het client object
+module.exports = {
+  client
+};
+
+
+
+
+// gebruik EJS als de standaard view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+
+
+// importeer routebestanden
+const alexiaRoutes = require('./routes/alexiaroutes');
+const larsRoutes = require('./routes/larsroutes');
+const lynnRoutes = require('./routes/lynnroutes');
+const thijmenRoutes = require('./routes/thijmenroutes');
+const youriRoutes = require('./routes/youriroutes');
+const algemeneroutes = require('./routes/algemeneroutes');
+
+
+
+
+// gebruik de geïmporteerde routes als middleware
+app.use(alexiaRoutes);
+app.use(larsRoutes);
+app.use(lynnRoutes);
+app.use(thijmenRoutes);
+app.use(youriRoutes);
+app.use(algemeneroutes);
+
+
+
+
+// error 404
+app.use(function (req, res) {
+  res.locals.title = "Error 404"
+  res.status(404).render('404', {
+    path: 'Error'
+  });
+});
+
+
 
 
 
