9781c9330bd2b2938e5f5576a6a9e41bd705cbbf
Alexiatech <alexiawiersma@gmail.com>
2023-04-02 17:31:03 +0200

    routes

--- routes/alexiaroutes.js
+++ routes/alexiaroutes.js
@@ -2,7 +2,10 @@ const { client } = require('../server');
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const { start } = require('repl');
+const {start} = require('repl');
+
+
+const bcrypt = require ('bcrypt');
 
 
 
@@ -17,38 +20,58 @@ router.use('/static/styles', express.static(path.join(__dirname, '../static/styl
 const moviesUser = client.db('Moviemates').collection('Users');
 
 router.get('/signup', (req, res) => {
-  res.render('signup', { title: 'sign in' });
+  res.render('signup', { title: 'sign in'});
 });
 
 router.get('/register', (req, res) => {
-  res.render('register', { title: 'register' });
+  res.render('register', { title: 'register'});
 });
 
 
 
+// registreren voor de website
 
 router.post('/submit', async (req, res) => {
-  const name = req.body.name;
-  const birthday = req.body.birthday;
-  const email = req.body.email;
-  const password = req.body.password;
+      const name = req.body.name;
+      const birthday = req.body.birthday;
+      const email = req.body.email;
+      const password = req.body.password;
+      // const confirmPassword = req.body.password2;
+      // const error = "Password don't match";
+
+      console.log(name, email, password, birthday);
+      
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+        const userdata = {
+          name: name,
+          pwd: hashedPassword,
+          email: email,
+          birthday: birthday,
+        }
+
+        console.log(userdata);
+        await moviesUser.insertOne(userdata);
+
+        // redirect the user to the confirmation page
+        res.redirect('/signup');
+
+      
+    });
 
 
-  console.log(name, email, password, birthday);
+    // express session aanmelden 
 
-    const userdata = {
-      name: name,
-      pwd: password, 
-      email: email,
-      birthday: birthday,
-    }
 
-    console.log(userdata);
-    await moviesUser.insertOne(userdata);
 
-    // redirect the user to the confirmation page
-    res.redirect('/signup');
-  
-});
 
-module.exports = router;
+
+
+
+
+
+    module.exports = router;
+
+
+    
+
